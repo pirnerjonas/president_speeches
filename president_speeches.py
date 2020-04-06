27d349b518780c3261c019dd39bd9b634e70bc7b
Jonas Pirner <48887249+pirnerjonas@users.noreply.github.com>
2020-04-06 12:55:12 +0200

    Minor changes to plots

--- president_speeches.py
+++ president_speeches.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import nltk
+import re
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -50,37 +51,43 @@ def plot_most_frequent_nouns(president_topics, TOP_N):
     color_list[14] = color_war
     ax.barh(list(aspects), list(values),color=color_list)
     ax.set_xlabel('Frequency')
+    ax.set_title('Most common nouns/topics')
     fig.tight_layout()
     fig.savefig('images/most_frequent_nouns.png')
 
-def plot_war_and_peace_time(tfidf_df):
+def plot_war_and_peace_time(data):
     fig, ax = plt.subplots(figsize=(12,4))
-    ax.plot(tfidf_df.loc['war',:], color=color_war)
-    ax.plot(tfidf_df.loc['peace',:], color=color_peace)
+    ax.plot(data.loc['war',:], color=color_war)
+    ax.plot(data.loc['peace',:], color=color_peace)
     ax.legend()
     ax.set_ylabel('TfIdf Score')
     ax.annotate('War of 1812', xy=('1817', 0.35))
     ax.annotate('American Civil War', xy=('1869', 0.37))
-    ax.annotate('World War II', xy=('1941', 0.22))
-    ax.annotate('Vietnam War', xy=('1965', 0.34))
+    #ax.annotate('World War I', xy=('1909', 0.18))
+    ax.annotate('World War II', xy=('1937', 0.22))
+    ax.annotate('Vietnam War', xy=('1965', 0.35))
     ax.legend(title='Noun', shadow=True, fancybox=True)
+    ax.set_title('Importance of war and peace over time')
     plt.xticks(rotation=45)
     fig.tight_layout()
     fig.savefig('images/war_and_peace_time.png')
 
-def plot_words_peek_years(tfidf_df):
+def plot_words_peek_years(data, TOP_N):
+    president_list = ['Madison', 'Lincoln', 'Eisenhower','Nixon']
     year_list = [1813, 1865, 1957, 1973]
     color_list = [color_war, color_war, color_peace, color_peace]
     row_ind = [0,0,1,1]
     col_ind = [0,1,0,1]
-    TOP_N = 10
     fig, ax = plt.subplots(ncols=2,nrows=2, sharex=True)
-    for year, color, i, j in zip(year_list, color_list, row_ind, col_ind):
-        d = tfidf_df.loc[:,str(year)].sort_values(ascending=False).reset_index()[:TOP_N]
+    for president, year, color, i, j in zip(president_list, year_list, 
+                                            color_list, row_ind, col_ind):
+        # top words with corresponding tfidf
+        d = data.loc[:,str(year)].sort_values(ascending=False).reset_index()[:TOP_N]
         ax[i,j].barh(d.iloc[:,0],d.iloc[:,1],color=color)
-        ax[i,j].set_title('{}'.format(year))
+        ax[i,j].set_title(f'{president} {year}')
     ax[1,1].set_xlabel('tfidf score')
     ax[1,0].set_xlabel('tfidf score')
+    ax.set_title('Most important nouns/topics for peeks')
     fig.tight_layout()
     fig.savefig('images/peek_years.png')
 
@@ -91,6 +98,6 @@ president_topics = [extract_topics(speech) for speech in president_speeches]
 tfidf_df = create_tfidf(president_topics)
 
 # create the plots
-plot_most_frequent_nouns(president_topics,16)
+plot_most_frequent_nouns(president_topics, TOP_N=16)
 plot_war_and_peace_time(tfidf_df)
-plot_words_peek_years(tfidf_df)
+plot_words_peek_years(tfidf_df, TOP_N=8)
